45bd80bb732cb4e95ce3467c2133d7f3ade9a690
avinash <auskamalla06@gmail.com>
2021-12-25 14:08:15 +0530

    first_commit

---
+++ src/main/java/gitcode/Welcome.java
@@ -0,0 +1,13 @@
+package gitcode;
+
+public class Welcome {
+
+	// Your First Program
+
+	
+	    public static void main(String[] args) {
+	        System.out.println("Hello, World!"); 
+	    }
+	
+	
+}
